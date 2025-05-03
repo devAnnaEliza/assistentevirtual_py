5e58cd6a5bc2994f48210d90e508253a5b5146ce
Anna Eliza <annaeliza.als@gmail.com>
2025-05-02 21:58:23 -0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -9,7 +9,7 @@ import os
 audio = sr.Recognizer()
 maquina = pyttsx3.init()
 
-maquina.say("Olá, eu sou a Luzia, Como posse te ajudar?")
+maquina.say("Olá, eu sou a Luzia, Como posso te ajudar?")
 maquina.runAndWait()
 
 with sr.Microphone() as source:
