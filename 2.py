ec34e7f0bbbd6498fe76a788a1c7610106c458f7
Anna Eliza <annaeliza.als@gmail.com>
2025-05-02 21:57:50 -0300

    Update 2.py

---
+++ 2.py
@@ -0,0 +1,26 @@
+import speech_recognition as sr
+import pyttsx3
+import datetime 
+import webbrowser
+import wikipedia
+#import pywhatkit#
+import os
+
+audio = sr.Recognizer()
+maquina = pyttsx3.init()
+
+maquina.say("Olá, eu sou a Luzia, Como posse te ajudar?")
+maquina.runAndWait()
+
+with sr.Microphone() as source:
+    print("Ouvindo...")
+    voz = audio.listen(source)
+    comando = audio.recognize_google(voz, language = "pt-BR")
+    comando = comando.lower()
+    print(comando)
+
+    if 'luzia' in comando:
+        comando = comando.replace('luzia', '')
+        maquina.say(comando)
+        print(comando)
+        maquina.runAndWait()
