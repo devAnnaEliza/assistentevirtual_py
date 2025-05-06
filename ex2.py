3c3128c77c63f9eaf954af1102718e015c0f06a3
Anna Eliza <annaeliza.als@gmail.com>
2025-05-05 22:00:37 -0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -11,28 +11,9 @@ maquina = pyttsx3.init()
 maquina.say("Olá, eu sou a Luzia, Como posso te ajudar?")
 maquina.runAndWait()
 
-def executar_comando():
-
-    try:
-        with sr.Microphone() as source:
-            print("Ouvindo...")
-            voz = audio.listen(source)
-            comando = audio.recognize_google(voz, language = "pt-BR")
-            comando = comando.lower()
-            print(comando)
-
-            if 'luzia' in comando:
-                comando = comando.replace('luzia', '')
-                #maquina.say(comando)
-                print(comando)
-                maquina.runAndWait()
-
-    except:
-        print("Microfone não encontrado")
-
-    return comando
-
 def falar(texto):
-    
+    maquina.say(texto)
+    maquina.runAndWait()
+
 
 #add loop para perguntar qlqr coisa e add um break pra poder sair do loop e encerrar o assistente#
